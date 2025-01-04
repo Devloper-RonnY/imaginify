41176cdc51dcd7ac22a331d5289619c49c4b208b
roshan pawar <roshanpawar48640@gmail.com>
2025-01-04 13:40:22 +0530

    adding mobile nav bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@
 const Home = () => {
   return (
     <div>
-      <p>Home</p>
+      {/* <p>Home</p> */}
     </div>
   );
 };
--- components/shared/sidebar.tsx
+++ components/shared/sidebar.tsx
@@ -19,7 +19,7 @@ const Sidebar = () => {
             </Link>
 
             <nav className='sidebar-nav'>
-        <SignedIn>
+            <SignedIn>
             <ul className='sidebar-nav_elements'>
             {navLinks.slice(0,6).map((link) => {
             const isActive = link.route === pathname
